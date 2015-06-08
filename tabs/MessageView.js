61f1d7a594796c42de39fd888d52a5bb5ee00883
yangtao <yangtao@yangtaodeMacBook-Pro.local>
2015-06-08 20:14:44 +0800

    issue:初始化

---
+++ tabs/MessageView.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+
+var {
+    StyleSheet,
+    Text,
+    View
+} = React;
+
+var messageView = React.createClass({
+    render: function() {
+        var user = this.props.user;
+        return (
+            <View style={ styles.container }>
+                <Text>Chat with { user.firstName } { user.lastName }</Text>
+            </View>
+        );
+    }
+});
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingTop: 64
+    }
+});
+
+module.exports = messageView
